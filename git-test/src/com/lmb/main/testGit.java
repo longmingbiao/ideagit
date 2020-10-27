905c7db271b5a22fa5e32872e3ad871177bc517c
longmingbiao <1272853934@qq.com>
2020-10-27 11:42:10 +0800

    修改testGit

--- git-test/src/com/lmb/main/testGit.java
+++ git-test/src/com/lmb/main/testGit.java
@@ -3,6 +3,7 @@ package com.lmb.main;
 public class testGit {
     public static void main(String[] args) {
         System.out.println("用idea连接git");
+        System.out.println("克隆下来了");
     }
     private void test(){
         int i = 0;
