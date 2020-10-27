a106e90520dd5cc3bd403104b089dffdcc6b6ff3
longmingbiao <1272853934@qq.com>
2020-10-27 11:12:32 +0800

    第二次修改提交

--- git-test/src/com/lmb/main/testGit.java
+++ git-test/src/com/lmb/main/testGit.java
@@ -4,4 +4,8 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("用idea连接git");
     }
+    private void test(){
+        int i = 0;
+        i+=10;
+    }
 }
